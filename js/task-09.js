937a2c511987669fe20c2303cc387f00d07dda5b
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-04 23:11:00 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,11 @@
+const bodyBackground = document.querySelector("body")
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+bodyBackground.addEventListener("click", handleClick);
+function handleClick() {
+  bodyBackground.style.backgroundColor = getRandomHexColor();
+};
