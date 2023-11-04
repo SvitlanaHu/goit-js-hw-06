dc81626227058f600a4bd521d65f164405ef1a97
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-04 18:39:01 +0200

    task-05 +trim()

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", (event) => {
-    nameOutput.textContent = event.currentTarget.value;
+    nameOutput.textContent = event.currentTarget.value.trim();
     if (nameOutput.textContent === "") {
         nameOutput.textContent = "Anonymous";
         return nameOutput;
