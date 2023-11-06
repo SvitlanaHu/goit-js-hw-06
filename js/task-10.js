3ccfe0cec8877c4f3f8a15e9cbee856c3f1f573b
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-06 21:37:36 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,44 +1,45 @@
-const boxes = document.querySelector("#boxes");
-const btnCreate = document.querySelector("button[data-create]");
-const btnDestroy = document.querySelector("button[data-destroy]");
-const inputNumber = document.querySelector("input");
-boxes.style.display = "flex";
-boxes.style.flexWrap = "wrap";
-boxes.style.padding = "20px";
-boxes.style.backgroundColor = "gray";
+const inputNumber = document.querySelector('input[type="number"]');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+boxesContainer.style.display = "flex";
+boxesContainer.style.flexWrap = "wrap";
+boxesContainer.style.padding = "20px";
+boxesContainer.style.backgroundColor = "gray";
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+inputNumber.addEventListener('input', handleInput);
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
 
-inputNumber.addEventListener("change", handleInput);
 function handleInput(event) {
-  const amount = event.currentTarget.value;
+  const amount = event.target.value;
   console.log(amount);
   return amount;
 }
 
-btnCreate.addEventListener("click", createBoxes);
-function createBoxes(amount) {
-  const number = amount.currentTarget.value;
-  let widthItem = 20;
+function createBoxes() {
+  const amount = handleInput();
+  let widthItem = 30;
+  let heightItem = 30;
+  let boxesMarkup = '';
 
-  // boxes.reduce.
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < amount; i++) {
+    const color = getRandomHexColor();
+    const boxMarkup = `<div style="width:${widthItem}px; height:${heightItem}px; background-color:${color}"></div>`;
+    boxesMarkup += boxMarkup;
 
-    const listDiv = boxes.createElement("div");
     widthItem += 10;
-    listDiv.style.width = `${widthItem}px`;
-    listDiv.style.height = `${widthItem}px`;
-    listDiv.children.style.backgroundColor = getRandomHexColor();
-    listDiv.append(listDiv)
-    console.log(listDiv);
+    heightItem += 10;
   }
+
+  boxesContainer.innerHTML = boxesMarkup;
 }
 
-btnDestroy.addEventListener("click", destroyBoxes);
 function destroyBoxes() {
-  boxes.length = 0;
+  // boxesContainer.innerHTML = '';
+  boxesContainer.length = 0;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
