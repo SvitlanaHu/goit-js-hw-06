69f1a16808cf05b6b1822404f9f733f6ebe8657e
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-08 00:51:37 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const textSpan = document.querySelector("#text");
+textSpan.style.fontSize = "56px";
 fontSizeControl.addEventListener("input", setOutput);
 function setOutput(event) {
     const eventValue = event.currentTarget.value;
-    textSpan.style.fontSize = `${eventValue}px`
+    textSpan.style.fontSize = `${eventValue}px`;
 }
